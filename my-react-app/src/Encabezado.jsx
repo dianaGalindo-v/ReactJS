2841aba45209177f04e11437109285b4fbfd5f5b
Diana Galindo Vergara <240360@utxicotepec.edu.mx>
2026-01-23 14:04:58 -0600

    created: Encabezado.jsx y se agregaron imagenes (logos) y funciones como: Encabezado, Logo. Menu y Redes

---
+++ my-react-app/src/Encabezado.jsx
@@ -0,0 +1,53 @@
+import miLogo from './assets/logo.png';
+import facebook from './assets/facebook.png';
+import instagram from './assets/instagram.png';
+import gmail from './assets/gmail.png';
+import tiktok from './assets/tik-tok.png';
+
+function Encabezado(){
+    return (
+        <div>
+            <Logo />
+            <Menu />
+            <Redes />
+            <h2>Bienvenido a mi sitio</h2>
+        </div>
+    );
+}
+
+function Logo(){
+    return (
+        <div>
+            <img src={miLogo} alt="React Logo" />
+        </div>
+    );
+}
+
+
+function Menu(){
+    return (
+        <nav>
+            <ul>
+                <li>Inicio</li>
+                <li>Acerca de</li>
+                <li>Productos</li>
+                <li>Contacto</li>
+                <li>Sucursales</li>
+            </ul>
+        </nav>
+    );
+}
+
+function Redes(){
+    return (
+        <nav>
+            <ul>
+                <li><img src={facebook} alt="Facebook" /></li> 
+                <li><img src={instagram} alt="Instagram" /></li>
+                <li><img src={gmail} alt="Gmail" /></li>
+                <li><img src={tiktok} alt="TikTok" /></li>
+            </ul>
+        </nav>
+    );
+}
+export default Encabezado
